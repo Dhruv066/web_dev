d6ad593dd6f38f900f1b84936254e09ac228520c
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-07-09 16:00:21 +0530

    authentication and authorization

--- NodeJs/cookies and sessions/controllers/authController.js
+++ NodeJs/cookies and sessions/controllers/authController.js
@@ -2,9 +2,11 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     pageTitle: "Login",
     currentPage: "login",
-    isLoggedIn: false
+    isLoggedIn: false,
   });
 };
+
+
 
 exports.postLogin = (req, res, next) => {
   console.log(req.body);
@@ -12,10 +14,10 @@ exports.postLogin = (req, res, next) => {
   //res.cookie("isLoggedIn", true);
   //req.isLoggedIn = true;
   res.redirect("/");
-}
+};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(() => {
     res.redirect("/login");
-  })
-}
+  });
+};
