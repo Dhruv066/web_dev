3d4e81364fac4f8d8d61f5433c576ed20390ebc3
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-07-24 21:28:19 +0530

    resume section portfolio

--- react/self practice/portfolio/src/components/Resume/Resume.jsx
+++ react/self practice/portfolio/src/components/Resume/Resume.jsx
@@ -1,14 +1,50 @@
-import style from './Resume.module.css';
+import style from "./Resume.module.css";
 import { RiProfileLine } from "@remixicon/react";
 const Resume = () => {
   return (
     <div className={style.container}>
       <h1 className={style.heading}>
-        <RiProfileLine size={28} /> 
-         Resume
+        <RiProfileLine size={28} />
+        Resume
       </h1>
+      <h6 className={style.h6}>Education</h6>
+      <hr />
+      <div className={style.boxcontainer}>
+        <div className={style.box}>
+          <span>2026 - Present</span>
+          <h3>B.Tech in Computer Science & Artificial Intelligence</h3>
+          <h5>Dr. A. P. J. Abdul Kalam Technical University, Lucknow</h5>
+          <p>
+            Focusing on core computer science fundamentals, machine learning
+            algorithms, and intelligent systems. Active participant in coding
+            clubs and hackathons.
+          </p>
+        </div>
+        <div className={style.box}>
+          <span>2024</span>
+          <h3>Intermediate</h3>
+
+          <h5>
+            Doon International Sen. Sec. School, Palia Kalan <span>(CBSE)</span>
+          </h5>
+          <p>
+            
+          </p>
+        </div>
+        <div className={style.box}>
+          <span>2022</span>
+          <h3>High School</h3>
+
+          <h5>
+            Doon International Sen. Sec. School, Palia Kalan <span>(CBSE)</span>
+          </h5>
+          <p>
+            
+          </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Resume
+export default Resume;
---
+++ react/self practice/portfolio/src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import style 
+
+const Contact = () => {
+  return (
+    <div className={style.container}>
+      
+    </div>
+  )
+}
+
+export default Contact
